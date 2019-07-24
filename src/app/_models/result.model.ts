d3d3361eb9162b64bed14c3d286bc4f287e26943
Carlos Guilherme Felismino Pedroni <calosguilherme@hotmail.com>
2019-07-24 01:20:55 -0300

    Commit inicial

---
+++ src/app/_models/result.model.ts
@@ -0,0 +1,8 @@
+import { User, ConceptMap, Version } from "./index.model";
+export interface Result{
+    userMessage: string,
+    user?: User,
+    map?: ConceptMap,
+    version?: Version,
+    url?:string
+};
---
+++ src/app/global.vars.ts
@@ -0,0 +1,7 @@
+export const authApiUri: string = 'http://cmpaas.org/v1/auth';
+export const userApiUri: string = 'http://cmpaas.org/v1/users';
+export const mapApiUri: string = 'http://cmpaas.org/v1/maps';
+export const meApiUri: string = 'http://cmpaas.org/v1/users/me';
+export const versionApiUri: string = 'http://cmpaas.org/v1/versions';
+export const followApiUri: string = 'http://cmpaas.org/v1/follow';
+export const groupApiUri: string = 'http://cmpaas.org/v1/groups';
