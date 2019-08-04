b56f07df7b5773a5d581e7926a5d63b263d419f6
Unknown <calosguilherme@hotmail.com>
2019-08-03 21:32:43 -0300

    v7

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(__dirname +'/dist/cmpaas-frontend'));
  
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+    res.sendFile(path.join(__dirname + '/dist/cmpaas-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
