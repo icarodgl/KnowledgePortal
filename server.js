435c2d34715b56ff03e107e40689d9526716e393
Unknown <calosguilherme@hotmail.com>
2019-08-03 21:04:11 -0300

    v3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname +'/dist/cmpaas-frontend'));
  
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/cmpaas-fronten/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cmpaas-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
